43cb6119738e40e43b0b870c934e41ea570d568b
mofe64 <ogunbiyioladapo33@gmail.com>
2020-05-19 12:09:08 +0100

    initial setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const methodoOverride = require('method-override');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+
+app.use(express.json({ limit: '10kb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
+
+app.use(cookieParser());
+
+module.exports = app;
